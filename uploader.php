bae246a533b6c573b1d7099c37d7382913a6d9eb
polmes <pol.developer1@gmail.com>
2016-10-08 01:58:16 +0200

    fixed move uploaded file

--- uploader.php
+++ uploader.php
@@ -3,5 +3,9 @@
 print_r( $_POST );
 print_r( $_FILES );
 
-// Check size, security, etc.
-move_uploaded_file( $_FILES['tmp_name'], '/var/www/dev.coderagora.com/crispy-data' );
+// Should check size, security, etc.
+if ( move_uploaded_file( $_FILES['tmp_name'], '/var/www/dev.coderagora.com/crispy-data/' . $_FILES['name'] ) ) {
+	echo "Success! Maybe?";
+} else {
+	die( "There was an unexpected error" );
+}
