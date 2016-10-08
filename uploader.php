c15702284bd4e0df0f33fedd164db58428cfe699
polmes <pol.developer1@gmail.com>
2016-10-08 02:03:02 +0200

    try new path

--- uploader.php
+++ uploader.php
@@ -4,7 +4,7 @@ print_r( $_POST );
 print_r( $_FILES );
 
 // Should check size, security, etc.
-if ( move_uploaded_file( $_FILES['tmp_name'], '/var/www/dev.coderagora.com/crispy-data/' . $_FILES['name'] ) ) {
+if ( move_uploaded_file( $_FILES['tmp_name'], '/home/p0l/' . $_FILES['name'] ) ) {
 	echo "Success! Maybe?";
 } else {
 	die( "There was an unexpected error" );
