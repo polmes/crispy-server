fdf1823f76fe7aca1ec8cae5ec913d92826d535d
polmes <pol.developer1@gmail.com>
2016-10-08 18:09:18 +0200

    implemented headers again

--- downloader.php
+++ downloader.php
@@ -13,17 +13,18 @@ $statement = $connection->prepare( $query );
 $statement->bindParam( ':client_file', $client_file );
 $statement->execute();
 
-$row = $statement->fetch( PDO::FETCH_ASSOC );
-$server_file = $row['server_file'];
-print_r( $row );
-echo $server_file;
-echo "END\n";
+$rows = $statement->fetchAll( PDO::FETCH_ASSOC ); // could also use fetch()
+if ( count( $rows ) > 1 ) {
+	die( "Cannot have more than one file per path" );
+}
 
-/*if ( file_exists( $server_file ) ) {
+$server_file = $rows[0]['server_file'];
+
+if ( file_exists( $server_file ) ) {
 	// header( 'Accept-Ranges: bytes' ); // if we wanted resumable downloads, but files too small
 	header( 'Content-Type: application/octet-stream' ); // generic MIME
 	header( 'Content-Disposition: attachment; filename=' . basename( $server_file ) ); // C++ will remove .uniquid extension when file is transfered and checked via hash
 	header( 'Content-Length: ' . filesize( $server_file ) );
 	readfile( $server_file );
 	// exit;
-} else die( "File doesn't exist" );*/
+} else die( "File doesn't exist" );
