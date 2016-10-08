58b4209ef5a9af7c7197ed08ae23578270770cb3
polmes <pol.developer1@gmail.com>
2016-10-08 17:43:37 +0200

    switched to PDO on fetcher

--- db_list.php
+++ db_list.php
@@ -4,19 +4,19 @@ require_once( 'connect.php' );
 
 // print_r( $_POST );
 
-$user = mysqli_real_escape_string( $connection, $_POST['username'] );
-$client = mysqli_real_escape_string( $connection, $_POST['clientname'] );
+$user = $_POST['username'];
+$client = $_POST['clientname'];
 
 $query = "SELECT hash, server_file, file_" . $client . " FROM user_" . $user;
 // echo $query . "\n";
 
-$result = mysqli_query( $connection, $query );
-while ( $row = mysqli_fetch_assoc( $result ) ) {
+$statement = $connection->prepare( $query );
+$statement->execute();
+
+while ( $row = $statement->fetch( PDO::FETCH_ASSOC ) ) {
 	// print_r( $row );
 
 	// if null...
 	echo $row['hash'] . "\t" . $row['file_' . $client ] . "\t" . filemtime( $row['server_file'] ) . "\n";
 }
 // die...
-
-mysqli_close( $connection );
