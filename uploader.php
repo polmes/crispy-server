c6bedf53073c0adf098a32479b8d1e2fa496de73
polmes <pol.developer1@gmail.com>
2016-10-08 03:41:04 +0200

    test echos

--- uploader.php
+++ uploader.php
@@ -25,6 +25,9 @@ $statement = mysqli_prepare( $connection, $query );
 mysqli_stmt_bind_param( $statement, "s", $_POST['filePath'] );
 
 if ( mysqli_stmt_execute( $statement ) ) {
+	echo "If";
 	$result = mysqli_store_result( $connection );
 	echo mysqli_affected_rows( $connection );
+} else {
+	echo "No luck";
 }
