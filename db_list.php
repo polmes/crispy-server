9e9264e6a1ea213cd8e22366695f70ed7afc0d7a
polmes <pol.developer1@gmail.com>
2016-10-08 12:37:41 +0200

    added hash data and renamed file

--- db_list.php
+++ db_list.php
@@ -7,15 +7,15 @@ require_once( 'connect.php' );
 $user = mysqli_real_escape_string( $connection, $_POST['username'] );
 $client = mysqli_real_escape_string( $connection, $_POST['clientname'] );
 
-$query = "SELECT server_file, file_" . $client . " FROM user_" . $user;
+$query = "SELECT hash, server_file, file_" . $client . " FROM user_" . $user;
 // echo $query . "\n";
 
 $result = mysqli_query( $connection, $query );
 while ( $row = mysqli_fetch_assoc( $result ) ) {
 	// print_r( $row );
-	
+
 	// if null...
-	echo $row['file_' . $client ] . "\t" . filemtime( $row['server_file'] ) . "\n";
+	echo $row['hash'] . "\t" . $row['file_' . $client ] . "\t" . filemtime( $row['server_file'] ) . "\n";
 }
 // die...
 
