1fad570c8557a896ffec680d6ff6e376b956d08a
polmes <pol.developer1@gmail.com>
2016-10-08 02:23:48 +0200

    finally fixed upload, probably

--- uploader.php
+++ uploader.php
@@ -4,9 +4,9 @@ print_r( $_POST );
 print_r( $_FILES );
 
 // Should check size, security, etc.
-if ( move_uploaded_file( $_FILES['tmp_name'], '/var/www/dev.coderagora.com/crispy-data/' . $_FILES['name'] ) ) {
+if ( move_uploaded_file( $_FILES['file']['tmp_name'], '/var/www/dev.coderagora.com/crispy-data/' . $_FILES['file']['name'] ) ) {
 	echo "Success! Maybe?";
-} else if ( is_dir( '/var/www/dev.coderagora.com/crispy-data/' ) && is_writable( '/var/www/dev.coderagora.com/crispy-data/' ) ) {
+} else if  ( ! ( is_dir( '/var/www/dev.coderagora.com/crispy-data/' ) && is_writable( '/var/www/dev.coderagora.com/crispy-data/' ) ) ) {
 	die( "Write error" );
 } else {
 	die( "There was an unexpected error" );
