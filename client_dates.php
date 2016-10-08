f91bc12e6534511f27dbbd055fae9df9a5049aa3
polmes <pol.developer1@gmail.com>
2016-10-08 10:42:50 +0200

    fixed sql query

--- client_dates.php
+++ client_dates.php
@@ -7,8 +7,8 @@ print_r( $_POST );
 $user = mysqli_real_escape_string( $connection, $_POST['username'] );
 $client = mysqli_real_escape_string( $connection, $_POST['clientname'] );
 
-$query = "SELECT server_file, client_" . $client . " FROM user_" . $user;
-echo $query . '\n';
+$query = "SELECT server_file, file_" . $client . " FROM user_" . $user;
+echo $query . "\n";
 
 $result = mysqli_query( $connection, $query );
 while ( $row = mysqli_fetch_array( $result ) ) {
