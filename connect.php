ec1cb498a157ff675819d08196767ca4d26f759b
polmes <pol.developer1@gmail.com>
2016-10-08 02:52:47 +0200

    added DB connection file

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = parse_ini_file( '/var/www/dev.coderagora.com/config.ini' );
+if ( ! $config ) die( "Config access error" );
+
+$connection = mysqli_connect( $config['server'], $config['user'], $config['password'], $config['database'] );
+if ( ! $connection ) die( "DB connection error: " . mysqli_connect_error() );
+
+echo "Connected successfully\n";
