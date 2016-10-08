2ec9cee5d4c57c5a86c13eb07d6345993f554f34
polmes <pol.developer1@gmail.com>
2016-10-09 00:00:21 +0200

    send client_file

--- app-fetcher.php
+++ app-fetcher.php
@@ -5,7 +5,7 @@ require_once( 'connect.php' );
 $user = $_POST['username'];		
 $app = $_POST['app'];
 
-$query = "SELECT server_file FROM user_" . $user . " WHERE app = :app";
+$query = "SELECT client_file FROM user_" . $user . " WHERE app = :app";
 // echo $query;
 
 $statement = $connection->prepare( $query );
@@ -13,5 +13,5 @@ $statement->bindParam( ':app', $app );
 $statement->execute();
 
 while ( $row = $statement->fetch( PDO::FETCH_ASSOC ) ) {
-	echo $row['server_file'] . "\n";
+	echo $row['client_file'] . "\n";
 }
