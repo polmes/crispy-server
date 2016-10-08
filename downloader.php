922348d60bbe3f5baf0931b6ff866c268dfa2447
polmes <pol.developer1@gmail.com>
2016-10-08 17:55:06 +0200

    commented headers for debug

--- downloader.php
+++ downloader.php
@@ -15,12 +15,15 @@ $statement->execute();
 
 $row = $statement->fetch( PDO::FETCH_ASSOC );
 $server_file = $row['server_file'];
+print_r( $row );
+echo $server_file;
+echo "END\n";
 
-if ( file_exists( $server_file ) ) {
+/*if ( file_exists( $server_file ) ) {
 	// header( 'Accept-Ranges: bytes' ); // if we wanted resumable downloads, but files too small
 	header( 'Content-Type: application/octet-stream' ); // generic MIME
 	header( 'Content-Disposition: attachment; filename=' . basename( $server_file ) ); // C++ will remove .uniquid extension when file is transfered and checked via hash
 	header( 'Content-Length: ' . filesize( $server_file ) );
 	readfile( $server_file );
 	// exit;
-} else die( "File doesn't exist" );
+} else die( "File doesn't exist" );*/
