c4359a8ca59b5d08807ab0086fd4792175c4f557
polmes <pol.developer1@gmail.com>
2016-10-08 03:34:23 +0200

    mysqli statement

--- uploader.php
+++ uploader.php
@@ -11,3 +11,19 @@ if ( move_uploaded_file( $_FILES['file']['tmp_name'], '/var/www/dev.coderagora.c
 } else {
 	die( "There was an unexpected error" );
 }
+
+$user = mysqli_real_escape_string( $_POST['username'] );
+$client = mysqli_real_escape_string( $_POST['clientName'] );
+
+/* SAVE INFO IN DB */
+
+require_once( 'connect.php' );
+
+// Check if user is valid
+$query = "SELECT FROM user_" . $user . " WHERE file_" . $client . " = ?";
+$statement = mysqli_prepare( $connection, $query );
+mysqli_stmt_bind_param( $statement, "s", $_POST['filePath'] );
+
+if ( mysqli_stmt_execute( $statement ) ) {
+	echo mysqli_affected_rows( $connection );
+}
