b747e5f0f3edb0c8b080dc319ab46fbea35942a0
polmes <pol.developer1@gmail.com>
2016-10-08 02:18:37 +0200

    added more error checks

--- uploader.php
+++ uploader.php
@@ -6,6 +6,8 @@ print_r( $_FILES );
 // Should check size, security, etc.
 if ( move_uploaded_file( $_FILES['tmp_name'], '/var/www/dev.coderagora.com/crispy-data/' . $_FILES['name'] ) ) {
 	echo "Success! Maybe?";
+} else if ( is_dir( '/var/www/dev.coderagora.com/crispy-data/' ) && is_writable( '/var/www/dev.coderagora.com/crispy-data/' ) ) {
+	die( "Write error" );
 } else {
 	die( "There was an unexpected error" );
 }
