7f23db90d4b1c21d5ceda815c14c5cae86f0b21d
polmes <pol.developer1@gmail.com>
2016-10-08 10:38:08 +0200

    fetch rows

--- client_dates.php
+++ client_dates.php
@@ -2,13 +2,17 @@
 
 require_once( 'connect.php' );
 
+print_r( $_POST );
+
 $user = mysqli_real_escape_string( $connection, $_POST['username'] );
-$client = mysqli_real_escape_string( $connection, $_POST['clientName'] );
+$client = mysqli_real_escape_string( $connection, $_POST['clientname'] );
 
 $query = "SELECT server_file, client_" . $client . " FROM user_" . $user;
-echo $query;
+echo $query . '\n';
 
 $result = mysqli_query( $connection, $query );
-print_r( $result );
+while ( $row = mysqli_fetch_array( $result ) ) {
+	echo $row . '\n';
+}
 
 mysqli_close( $connection );
